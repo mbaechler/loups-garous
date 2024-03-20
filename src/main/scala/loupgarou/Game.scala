c4ad9f72de02d9bc299500757ae5c720edd5256c
Matthieu Baechler <matthieu@baechler-craftsmanship.fr>
2024-03-20 16:09:42 +0100

    step 13

--- src/main/scala/loupgarou/Game.scala
+++ src/main/scala/loupgarou/Game.scala
@@ -62,7 +62,7 @@ def loupsGarousAttaquent(village: Village)(using interaction: Interaction): (Vil
   (villageÀJour, Victime(victime))
 
 def déroulementDuJour(village: Village)(using interaction: Interaction): Village =
-  val maire = interaction.electionMaire(village)
+  val maire = village.maire.getOrElse(interaction.electionMaire(village))
   village.setMaire(maire)
 
 def jour(victime: Victime)(village: Village)(using interaction: Interaction): FinDePartie =
